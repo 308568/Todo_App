a2312f892ec64750461d46814a83acf51bf50b18
Sandhyarani Bhalke <140248987+308568@users.noreply.github.com>
2025-03-22 19:52:47 +0530

    first commit

---
+++ frontend/src/components/PageNotFound.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <>
+
+       <div className='flex flex-col h-screen items-center justify-center'>
+          
+          <div className='text-2xl'>404</div>
+          <br/>
+          <br/>
+          <div className='text-xl  font-semibold text-green-700'>Page Not Found</div>
+
+       </div>
+
+    </>
+  )
+}
+
+export default PageNotFound
